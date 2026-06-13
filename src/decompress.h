3990bb8117f78fe4f4892de2d4a0c5a990355f03
Linara Sankubaeva <linarasankubaeva55@mail.ru>
2026-06-13 22:28:19 +0300

    Adding comments to .h files.

--- src/decompress.h
+++ src/decompress.h
@@ -5,12 +5,20 @@
 typedef struct {
   uint8_t *symbol;
   uint8_t code_length;
-} HashDecodeEntry;
+} HashDecodeEntry; // Entry of a hash table for fast lookup of symbols using
+                   // codes
 
-int decompress_file(FILE *input, FILE *output);
-HuffmanTree *read_header(FILE *file, uint64_t *original_file_size_ptr);
-void recovering_codes(HuffmanTree *huffman_tree);
-int writing_decoded_file(FILE *input, FILE *output, HuffmanTree *huffman_tree,
-                         uint64_t original_file_size);
-
+int decompress_file(
+    FILE *input,
+    FILE *output); // Function for decompressing the input file to output file
+HuffmanTree *read_header(
+    FILE *file,
+    uint64_t *original_file_size_ptr); // Reading header and taking the data
+                                       // necessary for decompressing the file
+void recovering_codes(HuffmanTree *huffman_tree); // Recovering codes using the
+                                                  // data taken from header
+int writing_decoded_file(
+    FILE *input, FILE *output, HuffmanTree *huffman_tree,
+    uint64_t
+        original_file_size); // Decoding data using hash table for fast lookup
 #endif
--- src/huffman.h
+++ src/huffman.h
@@ -12,13 +12,13 @@ typedef struct Node {
   uint8_t symbol_length;
   struct Node *right;
   struct Node *left;
-} Node;
+} Node; // Nodes for Huffman table and priority queue
 
 typedef struct {
   Node **nodes;
   uint32_t size;
   uint32_t capacity;
-} PriorityQueue;
+} PriorityQueue; // Priority queue for building the Huffman tree
 
 typedef struct {
   Node *root;
@@ -27,7 +27,7 @@ typedef struct {
   uint8_t *code_lengths;
   uint32_t symbols_count;
   uint8_t symbol_length;
-} HuffmanTree;
+} HuffmanTree; // Huffman tree for counting code lengths
 
 typedef struct HashTableEntry {
   uint8_t *symbol_data;
@@ -36,36 +36,49 @@ typedef struct HashTableEntry {
   uint32_t code;
   uint8_t code_length;
   struct HashTableEntry *next;
-} HashTableEntry;
+} HashTableEntry; // Entry of hash table
 
 typedef struct {
   HashTableEntry **buckets;
   uint32_t size;
   uint32_t capacity;
-} HashTable;
+} HashTable; // Hash table for counting frequencies
 
-Node *create_node(uint8_t *symbol, uint32_t frequency, uint8_t symbol_length);
-void free_node(Node *node);
-uint8_t node_is_leaf(Node *node);
+Node *create_node(uint8_t *symbol, uint32_t frequency,
+                  uint8_t symbol_length); // Creating a node
+void free_node(
+    Node *node); // Freeing the data of a node (excluding symbol data)
+uint8_t node_is_leaf(Node *node); // Checking if the given node is a leaf
 
-PriorityQueue *pq_create(uint32_t capacity);
-void pq_push(PriorityQueue *priority_queue, Node *node);
-Node *pq_pop(PriorityQueue *priority_queue);
-Node *pq_merge(PriorityQueue *priority_queue);
-void pq_free(PriorityQueue *priority_queue);
+PriorityQueue *pq_create(uint32_t capacity); // Creating a priority queue
+void pq_push(PriorityQueue *priority_queue,
+             Node *node); // Pushing a node to priority queue
+Node *pq_pop(
+    PriorityQueue
+        *priority_queue); // Taking the last node of a priority queue, deleting
+                          // it from the priority queue and returning it
+Node *pq_merge(PriorityQueue *priority_queue); // Building the Huffman tree
+void pq_free(PriorityQueue *priority_queue);   // Freeing priority queue data
+                                               // (excluding nodes data)
 
-HuffmanTree *create_tree(Node *node, uint8_t symbol_length);
-void free_tree(HuffmanTree *tree);
+HuffmanTree *create_tree(Node *node,
+                         uint8_t symbol_length); // Creating a Huffman tree
+void free_tree(
+    HuffmanTree *tree); // Freeing the Huffman tree data (including symbol data)
 
-HashTable *create_hash_table(uint32_t capacity);
-void free_hash_table(HashTable *table);
-void free_hash_entry(HashTableEntry *entry);
+HashTable *create_hash_table(uint32_t capacity); // Creating hash table
+void free_hash_table(HashTable *table);          // Freeing hash table data
+void free_hash_entry(HashTableEntry *entry); // Freeing hash table entry data
 uint32_t hash_function(uint8_t *symbol, uint8_t symbol_length,
-                       uint32_t table_size);
+                       uint32_t table_size); // Hash function for counting
+                                             // frequencies using hash table
 void add_symbol_hash(HashTable *table, uint8_t *symbol, uint8_t symbol_length,
-                     uint32_t code, uint8_t code_length);
+                     uint32_t code,
+                     uint8_t code_length); // Adding symbol to hash table or
+                                           // incrementing frequency
 
-int compare_symbols(uint8_t *symbol1, uint8_t *symbol2, uint8_t symbol_length);
-int height_left(Node *node);
-
+int compare_symbols(uint8_t *symbol1, uint8_t *symbol2,
+                    uint8_t symbol_length); // Comparing symbols
+int height_left(
+    Node *node); // Counting height for deterministic Huffman tree building
 #endif
